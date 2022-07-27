62ca3a73653f9c4d9095cf9e5b19da75f9a2800f
dashakurilovich <Dasha23_@mail.ru>
2022-07-27 15:30:25 +0300

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router";
 import { Home } from './pages/Home/Home';
 import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
 import { Header } from './shader/Header/Header';
-import { Popup } from './shader/PopUp/Popup';
 
 function App() {
   return (
@@ -12,8 +11,9 @@ function App() {
       <div className="container">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/month-statistics" element={<MonthStatistics />} />
+          
+          <Route path="/The-weather/" element={<Home />} />
+          <Route path="/The-weather/month-statistics" element={<MonthStatistics />} />
         </Routes>
       </div>
     </div>
